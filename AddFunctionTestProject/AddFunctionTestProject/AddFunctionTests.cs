a992a258d9e0974e9a175584be971793f1e441f6
Hemant Kumar <hemantkumar@quantumphinance.com>
2022-04-17 23:20:09 +0530

    main : Added messages in Test Cases to identify which test case failed under one given function & implementation of Add function

--- AddFunctionTestProject/AddFunctionTestProject/AddFunctionTests.cs
+++ AddFunctionTestProject/AddFunctionTestProject/AddFunctionTests.cs
@@ -18,148 +18,185 @@ namespace AddFunctionTestProject
         public void SingleNumberSimpleTest()
         {
             int result = StringCalculator.Add("5");
-            Assert.AreEqual(5, result);
+            Assert.AreEqual(5, result, "Failed to Calculate Add for \"5\"");
 
             int result2 = StringCalculator.Add("0");
-            Assert.AreEqual(0, result2);
+            Assert.AreEqual(0, result2, "Failed to Calculate Add for \"0\"");
 
             int result3 = StringCalculator.Add("00");
-            Assert.AreEqual(0, result3);
+            Assert.AreEqual(0, result3, "Failed to Calculate Add for \"00\"");
         }
 
         [TestMethod]
         public void TwoNumbersSimpleTest()
         {
             int result = StringCalculator.Add("5,6");
-            Assert.AreEqual(11, result);
+            Assert.AreEqual(11, result, "Failed to Add two numbers, simple case : \"5,6\"");
 
             int result2 = StringCalculator.Add("0,1");
-            Assert.AreEqual(1, result2);
+            Assert.AreEqual(1, result2, "Failed to Add two numbers, simple case : \"0,1\"");
 
             int result3 = StringCalculator.Add("0,0");
-            Assert.AreEqual(0, result3);
+            Assert.AreEqual(0, result3, "Failed to Add two numbers, simple case : \"0,0\"");
 
             int result4 = StringCalculator.Add("000,00");
-            Assert.AreEqual(0, result4);
+            Assert.AreEqual(0, result4, "Failed to Add two numbers, simple case : \"000,00\"");
         }
 
         [TestMethod]
         public void AllowToAddMultipleNumbersTest()
         {
-            int result = StringCalculator.Add("1,2,3,4,5,6,6,7");
-            Assert.AreEqual(34, result);
+            string input = "1,2,3,4,5,6,6,7";
+            int result = StringCalculator.Add(input);
+            Assert.AreEqual(34, result, "Failed to add unknown amount of numbers, simple case: " + input);
 
-            int result2 = StringCalculator.Add("234,324,23,234,234,436,234,234,438,328,382,979,834");
-            Assert.AreEqual(4914, result2);
+            string input2 = "234,324,23,234,234,436,234,234,438,328,382,979,834";
+            int result2 = StringCalculator.Add(input2);
+            Assert.AreEqual(4914, result2, "Failed to add unknown amount of numbers, simple case: " + input2);
 
-            int result3 = StringCalculator.Add("0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
-            Assert.AreEqual(0, result3);
+            string input3 = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            int result3 = StringCalculator.Add(input3);
+            Assert.AreEqual(0, result3, "Failed to add unknown amount of numbers, simple case: " + input3);
 
-            int result4 = StringCalculator.Add("0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
-            Assert.AreEqual(1, result4);
+            string input4 = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            int result4 = StringCalculator.Add(input4);
+            Assert.AreEqual(1, result4, "Failed to add unknown amount of numbers, simple case: " + input4);
 
-            int result5 = StringCalculator.Add("000,00,0000,00001,00000");
-            Assert.AreEqual(1, result4);
+            string input5 = "000,00,0000,00001,00000";
+            int result5 = StringCalculator.Add(input5);
+            Assert.AreEqual(1, result4, "Failed to add unknown amount of numbers, simple case: " + input5);
         }
 
         [TestMethod]
         public void NewLineCharacterSimpleTest()
         {
-            int result = StringCalculator.Add("1,2\n3,4,5\n6,6\n7");
-            Assert.AreEqual(34, result);
+            string input = "1,2\n3,4,5\n6,6\n7";
+            int result = StringCalculator.Add(input);
+            Assert.AreEqual(34, result, "Failed to add numbers where new line character is used, simple case: " + input);
 
-            int result2 = StringCalculator.Add("234,324\n23,234,234,436,234,234,438\n328,382,979,834");
-            Assert.AreEqual(4914, result2);
+            string input2 = "234,324\n23,234,234,436,234,234,438\n328,382,979,834";
+            int result2 = StringCalculator.Add(input2);
+            Assert.AreEqual(4914, result2, "Failed to add numbers where new line character is used, simple case: " + input2);
 
-            int result3 = StringCalculator.Add("0,0,0,0,0,0,0,0,0,0,0\n0,0,0,0,0,0,0,0,0,0,0,0,0,0\n0,0,0,0,0,0,0,0,0,0\n0,0,0,0,0,0,0");
-            Assert.AreEqual(0, result3);
+            string input3 = "0,0,0,0,0,0,0,0,0,0,0\n0,0,0,0,0,0,0,0,0,0,0,0,0,0\n0,0,0,0,0,0,0,0,0,0\n0,0,0,0,0,0,0";
+            int result3 = StringCalculator.Add(input3);
+            Assert.AreEqual(0, result3, "Failed to add numbers where new line character is used, simple case: " + input3);
 
-            int result4 = StringCalculator.Add("0\n0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n1\n0\n0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n0");
-            Assert.AreEqual(1, result4);
+            string input4 = "0\n0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n1\n0\n0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n0";
+            int result4 = StringCalculator.Add(input4);
+            Assert.AreEqual(1, result4, "Failed to add numbers where new line character is used, simple case: " + input4);
 
-            int result5 = StringCalculator.Add("5\n5\n5\n50\n100\n500");
-            Assert.AreEqual(665, result5);
+            string input5 = "5\n5\n5\n50\n100\n500";
+            int result5 = StringCalculator.Add(input5);
+            Assert.AreEqual(665, result5, "Failed to add numbers where new line character is used, simple case: " + input5);
 
-            int result6 = StringCalculator.Add("0\n0");
-            Assert.AreEqual(665, result6);
+            string input6 = "0\n0";
+            int result6 = StringCalculator.Add(input6);
+            Assert.AreEqual(0, result6, "Failed to add numbers where new line character is used, simple case: " + input6);
 
-            int result7 = StringCalculator.Add("1\n0");
-            Assert.AreEqual(1, result7);
+            string input7 = "1\n0";
+            int result7 = StringCalculator.Add(input7);
+            Assert.AreEqual(1, result7, "Failed to add numbers where new line character is used, simple case: " + input7);
         }
 
         [TestMethod]
         public void ChangeDelimiterSimpleTest()
         {
-            int result = StringCalculator.Add("//;\n1;2;3;4;5;6;6;7");
-            Assert.AreEqual(34, result);
+            string input = "//;\n1;2;3;4;5;6;6;7";
+            int result = StringCalculator.Add(input);
+            Assert.AreEqual(34, result, "Failed to add numbers where delimiter is used, simple case: " + input);
 
-            int result2 = StringCalculator.Add("//.\n234.324.23.234.234.436.234.234.438.328.382.979.834");
-            Assert.AreEqual(4914, result2);
+            string input2 = "//.\n234.324.23.234.234.436.234.234.438.328.382.979.834";
+            int result2 = StringCalculator.Add(input2);
+            Assert.AreEqual(4914, result2, "Failed to add numbers where delimiter is used, simple case: " + input2);
 
-            int result3 = StringCalculator.Add("//;\n0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0");
-            Assert.AreEqual(0, result3);
+            string input3 = "//;\n0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0";
+            int result3 = StringCalculator.Add(input3);
+            Assert.AreEqual(0, result3, "Failed to add numbers where delimiter is used, simple case: " + input3);
 
-            int result4 = StringCalculator.Add("//;\n0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0");
-            Assert.AreEqual(1, result4);
+            string input4 = "//;\n0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0";
+            int result4 = StringCalculator.Add(input4);
+            Assert.AreEqual(1, result4, "Failed to add numbers where delimiter is used, simple case: " + input4);
 
-            int result5 = StringCalculator.Add("//)\n5)5)5)50)100)500");
-            Assert.AreEqual(665, result5);
+            string input5 = "//)\n5)5)5)50)100)500";
+            int result5 = StringCalculator.Add(input5);
+            Assert.AreEqual(665, result5, "Failed to add numbers where delimiter is used, simple case: " + input5);
         }
 
         [TestMethod]
         public void ChangeDelimiterNewLineTest()
         {
-            int result = StringCalculator.Add("//;\n1;2;3;4;5\n6;6\n7");
-            Assert.AreEqual(34, result);
+            string input = "//;\n1;2;3;4;5\n6;6\n7";
+            int result = StringCalculator.Add(input);
+            Assert.AreEqual(34, result, "Failed to add numbers where combination of delimiter & new line character is used, case : " + input);
 
-            int result2 = StringCalculator.Add("//.\n234\n324.23\n234.234.436.234.234.438\n328.382.979\n834");
-            Assert.AreEqual(4914, result2);
+            string input2 = "//.\n234\n324.23\n234.234.436.234.234.438\n328.382.979\n834";
+            int result2 = StringCalculator.Add(input2);
+            Assert.AreEqual(4914, result2, "Failed to add numbers where combination of delimiter & new line character is used, case : " + input2);
 
-            int result3 = StringCalculator.Add("//;\n0;0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0\n0;0;0;0;0;0\n0;0;0");
-            Assert.AreEqual(0, result3);
+            string input3 = "//;\n0;0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0\n0;0;0;0;0;0\n0;0;0";
+            int result3 = StringCalculator.Add(input3);
+            Assert.AreEqual(0, result3, "Failed to add numbers where combination of delimiter & new line character is used, case : " + input3);
 
-            int result4 = StringCalculator.Add("//;\n0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0\n1;0;0;0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0;0;0;0");
-            Assert.AreEqual(1, result4);
+            string input4 = "//;\n0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0\n1;0;0;0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0;0;0;0";
+            int result4 = StringCalculator.Add(input4);
+            Assert.AreEqual(1, result4, "Failed to add numbers where combination of delimiter & new line character is used, case : " + input4);
 
-            int result5 = StringCalculator.Add("//)\n5\n5\n5\n50\n100\n500");
-            Assert.AreEqual(665, result5);
+            string input5 = "//)\n5\n5\n5\n50\n100\n500";
+            int result5 = StringCalculator.Add(input5);
+            Assert.AreEqual(665, result5, "Failed to add numbers where combination of delimiter & new line character is used, case : " + input5);
 
-            int result6 = StringCalculator.Add("//;\n1\n0");
-            Assert.AreEqual(1, result6);
+            string input6 = "//;\n1\n0";
+            int result6 = StringCalculator.Add(input6);
+            Assert.AreEqual(1, result6, "Failed to add numbers where combination of delimiter & new line character is used, case : " + input6);
+
+            string input7 = "//;\n";
+            int result7 = StringCalculator.Add(input7);
+            Assert.AreEqual(0, result7, "Failed to add numbers where combination of delimiter & new line character is used, case : " + input7);
         }
 
         [TestMethod]
         public void NegativeNumbersInStringTest()
         {
-            var exception = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("//;\n1;2;3;-4;5\n6;6\n7"); });
-            Assert.AreEqual("negatives not allowed: -4", exception.Message);
+            string input = "//;\n1;2;3;-4;5\n6;6\n7";
+            var exception = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input); });
+            Assert.AreEqual("negatives not allowed: -4", exception.Message, "Exception message not matched, case: " + input);
 
-            var exception2 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("//.\n-234\n324.-23\n234.234.436.234.234.438\n-328.382.-979\n-834"); });
-            Assert.AreEqual("negatives not allowed: -234,-23,-328,-979,-834", exception2.Message);
+            string input2 = "//.\n-234\n324.-23\n234.234.436.234.234.438\n-328.382.-979\n-834";
+            var exception2 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input2); });
+            Assert.AreEqual("negatives not allowed: -234,-23,-328,-979,-834", exception2.Message, "Exception message not matched, case: " + input2);
 
-            var exception3 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("//;\n0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0\n-1;0;0;0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0;0;0;0"); });
-            Assert.AreEqual("negatives not allowed: -1", exception3.Message);
+            string input3 = "//;\n0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0\n-1;0;0;0;0;0;0;0;0;0;0\n0;0;0;0;0;0;0;0;0;0;0;0;0";
+            var exception3 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input3); });
+            Assert.AreEqual("negatives not allowed: -1", exception3.Message, "Exception message not matched, case: " + input3);
 
-            var exception4 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("//)\n-5\n-5\n-5\n-50\n-100\n-500"); });
-            Assert.AreEqual("negatives not allowed: -5,-5,-5,-50,-100,-500", exception4.Message);
+            string input4 = "//)\n-5\n-5\n-5\n-50\n-100\n-500";
+            var exception4 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input4); });
+            Assert.AreEqual("negatives not allowed: -5,-5,-5,-50,-100,-500", exception4.Message, "Exception message not matched, case: " + input4);
 
-            var exception5 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("//;\n-1\n0"); });
-            Assert.AreEqual("negatives not allowed: -1", exception5.Message);
+            string input5 = "//;\n-1\n0";
+            var exception5 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input5); });
+            Assert.AreEqual("negatives not allowed: -1", exception5.Message, "Exception message not matched, case: " + input5);
 
-            var exception6 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("//;\n-1\n-5"); });
-            Assert.AreEqual("negatives not allowed: -1,-5", exception6.Message);
+            string input6 = "//;\n-1\n-5";
+            var exception6 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input6); });
+            Assert.AreEqual("negatives not allowed: -1,-5", exception6.Message, "Exception message not matched, case: " + input6);
 
-            var exception7 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("-8,-9"); });
-            Assert.AreEqual("negatives not allowed: -8,-9", exception7.Message);
+            string input7 = "-8,-9";
+            var exception7 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input7); });
+            Assert.AreEqual("negatives not allowed: -8,-9", exception7.Message, "Exception message not matched, case: " + input7);
 
-            var exception8 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("-847"); });
-            Assert.AreEqual("negatives not allowed: -847", exception8.Message);
+            string input8 = "-847";
+            var exception8 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input8); });
+            Assert.AreEqual("negatives not allowed: -847", exception8.Message, "Exception message not matched, case: " + input8);
 
-            var exception9 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("//;\n-1;2;3;-4;5\n6;6\n7"); });
-            Assert.AreEqual("negatives not allowed: -1,-4", exception9.Message);
+            string input9 = "//;\n-1;2;3;-4;5\n6;6\n7";
+            var exception9 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input9); });
+            Assert.AreEqual("negatives not allowed: -1,-4", exception9.Message, "Exception message not matched, case: " + input9);
 
-            var exception10 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add("1,2,3\n4,5,6,6,-7"); });
-            Assert.AreEqual("negatives not allowed: -7", exception10.Message);
+            string input10 = "1,2,3\n4,5,6,6,-7";
+            var exception10 = Assert.ThrowsException<Exception>(() => { StringCalculator.Add(input10); });
+            Assert.AreEqual("negatives not allowed: -7", exception10.Message, "Exception message not matched, case: " + input10);
         }
     }
 }
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -9,8 +9,31 @@ namespace StringCalculatorNameSpace
         {
             if (input == "")
                 return 0;
-            var nums = input.Split(',');
-            return nums.Sum(a => int.Parse(a));
+            char delimiter = ',';
+            if(input.StartsWith("//") && input.Length >= 4)
+            {
+                delimiter = input[2];
+                input = input.Substring(4);
+            }
+            if (input == "")
+                return 0;
+            var numStrings = input.Split(delimiter, '\n');
+            int temp;
+            if(numStrings.All(a => int.TryParse(a, out temp)))
+            {
+                if(numStrings.Any(a => int.Parse(a) < 0))
+                {
+                    var negativeNums = numStrings.Select(a => int.Parse(a)).Where(a => a < 0).ToList();
+                    throw new Exception("negatives not allowed: " + string.Join(",", negativeNums));
+                }
+                else
+                {
+                    int addition = numStrings.Select(a => int.Parse(a)).Sum();
+                    return addition;
+                }
+            }
+
+            throw new NotSupportedException("Input format not supported.");
         }
     }
 }
